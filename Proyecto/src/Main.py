4107a41c5286564f172c8bfbf4530ef2fcbfc479
RaviolPignolo <leandroperezpignolo@gmail.com>
2025-05-13 17:16:47 -0300

    en las lista sde campeon e item se eliminó el espacio 'details' debido a que no tendrán uso; champion_list_menu e item_list_menu ahora ambos muestran la lista en forma de tabla y se pueden usar las 4 flechas de dirección para moverse por las mismas; En pantalla_select se informa de la idea general de su uso; En pantalla_juego implementé una tabla con un identificador para cada cuadrilla, también agregé una imagen de Karthus para ir probando el movimiento de los campeónes; Se debe mejorar el código pues tarda un tiempo en acceder a los menues y a cargar el movimiento de la imagen del personaje.

--- Proyecto/src/Main.py
+++ Proyecto/src/Main.py
@@ -7,8 +7,8 @@ import sys
 
 pygame.init()
 
-PANTALLA_ANCHO = 1900
-PANTALLA_ALTO = 1080
+PANTALLA_ANCHO = 1280
+PANTALLA_ALTO = 720
 
 NEGRO = (0, 0, 0)
 BLANCO = (255, 255, 255)
@@ -16,30 +16,30 @@ GRIS = (128, 128, 128)
 
 # Lista de los campeones e items
 champions_list = [
-    {'name': 'Aatrox', 'details': 'Details about Aatrox', 'image': 'Proyecto/src/Assets/Images/Iconos/Aatrox_icon.png'},
-    {'name': 'Ahri', 'details': 'Details about Ahri', 'image': 'Proyecto/src/Assets/Images/Iconos/Ahri_icon.png'},
-    {'name': 'Akali', 'details': 'Details about Akali', 'image': 'Proyecto/src/Assets/Images/Iconos/Akali_icon.png'},
-    {'name': 'Akshan', 'details': 'Details about Akshan', 'image': 'Proyecto/src/Assets/Images/Iconos/Akshan_icon.png'},
-    {'name': 'Alistar', 'details': 'Details about Alistar', 'image': 'Proyecto/src/Assets/Images/Iconos/Alistar_icon.png'},
-    {'name': 'Ambessa', 'details': 'Details about Ambessa', 'image': 'Proyecto/src/Assets/Images/Iconos/Ambessa_icon.png'},
-    {'name': 'Amumu', 'details': 'Details about Amumu', 'image': 'Proyecto/src/Assets/Images/Iconos/Amumu_icon.png'},
-    {'name': 'Anivia', 'details': 'Details about Anivia', 'image': 'Proyecto/src/Assets/Images/Iconos/Anivia_icon.png'},
-    {'name': 'Caitlyn', 'details': 'Details about Caitlyn', 'image': 'Proyecto/src/Assets/Images/Iconos/Caitlyn_icon.png'},
-    {'name': 'Jhin', 'details': 'Details about Jhin', 'image': 'Proyecto/src/Assets/Images/Iconos/Jhin_icon.png'},
-    {'name': 'Karthus', 'details': 'Details about Karthus', 'image': 'Proyecto/src/Assets/Images/Iconos/Karthus_icon.png'},
-    {'name': 'Maokai', 'details': 'Details about Maokai', 'image': 'Proyecto/src/Assets/Images/Iconos/Maokai_icon.png'},
-    {'name': 'Nautilus', 'details': 'Details about Nautilus', 'image': 'Proyecto/src/Assets/Images/Iconos/Nautilus_icon.png'},
-    {'name': 'TahmKench', 'details': 'Details about TahmKench', 'image': 'Proyecto/src/Assets/Images/Iconos/TahmKench_icon.png'},
-    {'name': 'Twitch', 'details': 'Details about Twitch', 'image': 'Proyecto/src/Assets/Images/Iconos/Twitch_icon.png'},
+    {'name': 'Aatrox', 'image': 'Proyecto/src/Assets/Images/Iconos/Aatrox_icon.png'},
+    {'name': 'Ahri',  'image': 'Proyecto/src/Assets/Images/Iconos/Ahri_icon.png'},
+    {'name': 'Akali', 'image': 'Proyecto/src/Assets/Images/Iconos/Akali_icon.png'},
+    {'name': 'Akshan', 'image': 'Proyecto/src/Assets/Images/Iconos/Akshan_icon.png'},
+    {'name': 'Alistar', 'image': 'Proyecto/src/Assets/Images/Iconos/Alistar_icon.png'},
+    {'name': 'Ambessa', 'image': 'Proyecto/src/Assets/Images/Iconos/Ambessa_icon.png'},
+    {'name': 'Amumu', 'image': 'Proyecto/src/Assets/Images/Iconos/Amumu_icon.png'},
+    {'name': 'Anivia', 'image': 'Proyecto/src/Assets/Images/Iconos/Anivia_icon.png'},
+    {'name': 'Caitlyn', 'image': 'Proyecto/src/Assets/Images/Iconos/Caitlyn_icon.png'},
+    {'name': 'Jhin', 'image': 'Proyecto/src/Assets/Images/Iconos/Jhin_icon.png'},
+    {'name': 'Karthus', 'image': 'Proyecto/src/Assets/Images/Iconos/Karthus_icon.png'},
+    {'name': 'Maokai', 'image': 'Proyecto/src/Assets/Images/Iconos/Maokai_icon.png'},
+    {'name': 'Nautilus', 'image': 'Proyecto/src/Assets/Images/Iconos/Nautilus_icon.png'},
+    {'name': 'TahmKench', 'image': 'Proyecto/src/Assets/Images/Iconos/TahmKench_icon.png'},
+    {'name': 'Twitch', 'image': 'Proyecto/src/Assets/Images/Iconos/Twitch_icon.png'},
 ]
 items_list = [
-    {'name': 'Bloodthirster', 'details': 'Details about Bloodthirster', 'image': 'Proyecto/src/Assets/Images/Items/Bloodthirster_item.png'},
-    {'name': 'GuinsoosRageblade', 'details': 'Details about Guinsoo', 'image': 'Proyecto/src/Assets/Images/Items/GuinsoosRageblade_item.png'},
-    {'name': 'Opportunity', 'details': 'Details about Opportunity', 'image': 'Proyecto/src/Assets/Images/Items/Opportunity_item.png'},
-    {'name': 'JakShosTheProtean', 'details': 'Details about Jaksho', 'image': 'Proyecto/src/Assets/Images/Items/JakShoTheProtean_item.png'},
-    {'name': 'Thornmail', 'details': 'Details about Thornmail', 'image': 'Proyecto/src/Assets/Images/Items/Thornmail_item.png'},
-    {'name': 'SpiritVisage', 'details': 'Details about SpiritVisage', 'image': 'Proyecto/src/Assets/Images/Items/SpiritVisage_item.png'},
-    {'name': 'BlackfireTorch', 'details': 'Details about BlackfireTorch', 'image': 'Proyecto/src/Assets/Images/Items/BlackfireTorch_item.png'},
+    {'name': 'Bloodthirster', 'image': 'Proyecto/src/Assets/Images/Items/Bloodthirster_item.png'},
+    {'name': 'GuinsoosRageblade', 'image': 'Proyecto/src/Assets/Images/Items/GuinsoosRageblade_item.png'},
+    {'name': 'Opportunity', 'image': 'Proyecto/src/Assets/Images/Items/Opportunity_item.png'},
+    {'name': 'JakShosTheProtean', 'image': 'Proyecto/src/Assets/Images/Items/JakShoTheProtean_item.png'},
+    {'name': 'Thornmail', 'image': 'Proyecto/src/Assets/Images/Items/Thornmail_item.png'},
+    {'name': 'SpiritVisage', 'image': 'Proyecto/src/Assets/Images/Items/SpiritVisage_item.png'},
+    {'name': 'BlackfireTorch', 'image': 'Proyecto/src/Assets/Images/Items/BlackfireTorch_item.png'},
 ]
 
 
@@ -132,6 +132,11 @@ def champion_list_menu():
 def items_list_menu():
     selected_item = 0
     back_button_rect = pygame.Rect(10, 10, 100, 40) # Define un rectángulo para el botón de "Atrás"
+    item_per_row = 5
+    x_offset = 150
+    y_offset = 200
+    spacing_x = 140  # Espacio horizontal entre items
+    spacing_y = 120  # Espacio vertical entre filas
     
     while True:
         for event in pygame.event.get():
@@ -140,14 +145,23 @@ def items_list_menu():
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
-                    selected_item = (selected_item + 1) % len(items_list)
+                    if (selected_item + item_per_row) < len(items_list):
+                        selected_item += item_per_row
                 elif event.key == pygame.K_UP:
-                    selected_item = (selected_item - 1) % len(items_list)
+                    if (selected_item - item_per_row) >= 0:
+                        selected_item -= item_per_row
+                elif event.key == pygame.K_RIGHT:
+                    if (selected_item % item_per_row) < (item_per_row - 1) and (selected_item + 1) < len(items_list):
+                        selected_item += 1
+                elif event.key == pygame.K_LEFT:
+                    if (selected_item % item_per_row) > 0:
+                        selected_item -= 1
                 elif event.key == pygame.K_RETURN:
+                    # Aquí puedes mostrar los detalles del campeón seleccionado
                     items_details(items_list[selected_item])
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if back_button_rect.collidepoint(event.pos):
-                    return
+                    return  # Regresar al menú anterior
 
         pantalla.fill(NEGRO)
         
@@ -157,15 +171,23 @@ def items_list_menu():
         pantalla.blit(back_text, back_text_rect)
         
         for i, Item in enumerate(items_list):
+            # Calcula la posición en la cuadrícula
+            row = i // item_per_row
+            col = i % item_per_row
+            x_position = x_offset + col * spacing_x
+            y_position = y_offset + row * spacing_y
+            
+            # Cargar y dibujar la imagen del campeón
             image = pygame.image.load(Item['image'])
-            image_rect = image.get_rect(center=(100, 200 + i * 60))
+            image_rect = image.get_rect(center=(x_position, y_position))
             pantalla.blit(image, image_rect)
             
+            # Dibujar el nombre del campeón
             if i == selected_item:
                 text = font.render(Item['name'], True, BLANCO)
             else:
-                text = font.render(Item['name'], True, (100, 100, 100))
-            text_rect = text.get_rect(midleft=(150, 200 + i * 60))
+                text = font.render(Item['name'], True, GRIS)
+            text_rect = text.get_rect(midtop=(x_position, y_position + 40))
             pantalla.blit(text, text_rect)
         
         pygame.display.update()
@@ -236,6 +258,16 @@ def items_details(item):
         pygame.display.update()
 
 def pantalla_select():
+    """
+    La idea es que al darle a Inicio te lleve a ésta ventana, donde los 2 jugadores seleccionarán la composición de su equipo con los campeones.
+    Los campeones se almacenen en las lista para el equipo rojo y azul y luego te lleva a la pantalla de juego, donde los campones estarán ya ubicados
+    y en base a algun resultado de orden, debajo del mapa se verá de quién es el turno actual, con un marco rojo o azul para identificar a que equipo pertenece
+    y algun efecto para que sobresalte si es el que ocupa el turno actual.
+    Debajo de cada uno se verá la vida y maná/energia/furia que tiene.
+    
+    Más abajo y ocupando más espacio, se verá en detalle el icono, vida, maná/energia/furia, habilidades(con sus puntos), item y efectos.
+    Acompañado de opciones para usar alguna habilidad, movimiento, ataque básico, uso de item, o información sobre el campeón, sus habilidades e items.
+    """
     
     lista_rojo = []
     lista_azul = []
@@ -253,9 +285,18 @@ def pantalla_select():
         pygame.display.update()
 
 def pantalla_juego():
+    CELDA_ANCHO = 80
+    CELDA_ALTO = 80
+    MAX_CELDAS_X = PANTALLA_ANCHO // CELDA_ANCHO
+    MAX_CELDAS_Y = int(PANTALLA_ALTO / 1.5) // CELDA_ALTO
 
     arena_imagen = pygame.image.load('Proyecto/src/Assets/Images/Arena.png')
-    arena_imagen = pygame.transform.scale(arena_imagen, (PANTALLA_ANCHO, (PANTALLA_ALTO / 1.5)))
+    arena_imagen = pygame.transform.scale(arena_imagen, (PANTALLA_ANCHO, int(PANTALLA_ALTO / 1.5)))
+    
+    champion1_image = pygame.image.load('Proyecto/src/Assets/Images/Iconos/Karthus_icon.png')
+    champion1_image = pygame.transform.scale(champion1_image, (CELDA_ANCHO, CELDA_ALTO))
+    champion1_x = 3
+    champion1_y = 2
     
     while True:
         for event in pygame.event.get():
@@ -264,10 +305,35 @@ def pantalla_juego():
                 sys.exit()
             elif event.type == pygame.MOUSEMOTION:
                 print("Posición:", event.pos)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    if champion1_y < MAX_CELDAS_Y - 1:
+                        champion1_y += 1
+                elif event.key == pygame.K_UP:
+                    if champion1_y > 0:
+                        champion1_y -= 1
+                elif event.key == pygame.K_RIGHT:
+                    if champion1_x < MAX_CELDAS_X -1:
+                        champion1_x += 1
+                elif event.key == pygame.K_LEFT:
+                    if champion1_x > 0:
+                        champion1_x -= 1
                 
         pantalla.fill(NEGRO)
         pantalla.blit(arena_imagen, (0, 0))
         
+        for x in range(0, PANTALLA_ANCHO, CELDA_ANCHO):
+            for y in range(0, int(PANTALLA_ALTO / 1.5), CELDA_ALTO):
+                rect = pygame.Rect(x, y, CELDA_ANCHO, CELDA_ALTO)
+                pygame.draw.rect(pantalla, BLANCO, rect, 1)
+                
+                font = pygame.font.Font(None, 24)
+                text = font.render(f"{x // CELDA_ANCHO}, {y // CELDA_ALTO}", True, BLANCO)
+                pantalla.blit(text, (x + 5, y + 5))
+                
+        champion1_pos_x = champion1_x * CELDA_ANCHO
+        champion1_pos_y = champion1_y * CELDA_ALTO
+        pantalla.blit(champion1_image, (champion1_pos_x, champion1_pos_y))
         
                 
         pygame.display.update()
